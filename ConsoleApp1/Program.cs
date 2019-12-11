7b1d7dbfe74f539716590aedc63328155d36ac0d
hood1237 <super.hood3@yandex.by>
2019-12-11 14:38:46 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] objects = new object[]{24,"hello",23.14,'b'};
+            foreach(object obj in objects)
+            {
+                Console.Write($"{obj} ");
+            }
+            Console.WriteLine();
+            int k = int.Parse(Console.ReadLine());
+            rrotate(objects, k);
+            foreach(object obj in objects)
+            {
+                Console.Write($"{obj} ");
+            }
+            Console.ReadLine();
+        }
+
+        static object rrotate(object[] objects, int k)
+        {
+            if ( objects.Length > 1 )
+            {
+                int n = objects.Length;
+                k %= n;
+                
+                Array.Reverse(objects, 0, n - k);
+                Array.Reverse(objects, n - k, k);
+                Array.Reverse(objects);
+            }
+            return objects;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName;
+            string data;
+            Console.WriteLine("Input file name");
+            fileName = Console.ReadLine();
+            Console.WriteLine("Input data");
+            data = Console.ReadLine();
+            if(dsearch(fileName, data) != -1)
+            {
+                Console.WriteLine(dsearch(fileName, data));
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+            Console.ReadLine();
+        }
+
+        static int dsearch(string fileName, string data)
+        {
+            int buffSize = 256;
+            FileStream fileStream = new FileStream(fileName,FileMode.Open);
+            int fileLength = (int)fileStream.Length;
+            byte [] buffer = new byte[buffSize];
+            while(true)
+            {
+                int readed = 0;
+                int offset;
+                for(offset = 0; offset<fileStream.Length;)
+                {
+                    readed = fileStream.Read(buffer, 0, 256);
+                    if( readed == 0)
+                        break;
+                    offset += readed;
+                    string dataFromFile = System.Text.Encoding.Default.GetString(buffer);
+                    if(dataFromFile.Equals(data))
+                    {
+                        return offset;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
